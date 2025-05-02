b4474268e5fa541b9afb0af04d05cdefef069a57
sfauzii <sjahrulfauzii@gmail.com>
2025-05-02 20:42:16 +0700

    fixed error deploy

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Try to preload the SSL library if possible
+if (!extension_loaded('openssl')) {
+    @dl('openssl.so');
+}
 
 require __DIR__ . '/../public/index.php';
